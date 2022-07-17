29b57ab7e849c4479f1601cccc28b9745be0707a
ellonka <ella.trofimova2403@gmail.com>
2022-07-17 11:17:36 +0300

    third try to deploy

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -5,7 +5,7 @@ import io.javalin.Javalin;
 public class App {
     public static void main(String[] args) {
         Javalin app = getApp();
-        app.start(7070);
+        app.start(8080);
     }
 
     public static Javalin getApp() {
