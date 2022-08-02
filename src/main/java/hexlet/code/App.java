bd4b7c7de4883cbd8e6190fdbf67f3e78dd5642b
ellonka <ella.trofimova2403@gmail.com>
2022-08-02 09:03:25 +0300

    add thymeleaf and bootstrap

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,6 +1,11 @@
 package hexlet.code;
 
 import io.javalin.Javalin;
+import io.javalin.plugin.rendering.template.JavalinThymeleaf;
+import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 public final class App {
     public static void main(String[] args) {
@@ -11,6 +16,8 @@ public final class App {
     public static Javalin getApp() {
         Javalin app = Javalin.create(config -> {
             config.enableDevLogging();
+            config.enableWebjars();
+            JavalinThymeleaf.configure(getTemplateEngine());
         });
 
         addRoutes(app);
@@ -21,11 +28,23 @@ public final class App {
     }
 
     private static void addRoutes(Javalin app) {
-        app.get("/", ctx -> ctx.result("Hello world"));
+        app.get("/", ctx -> ctx.render("/index.html"));
     }
 
     private static int getPort() {
         String port = System.getenv().getOrDefault("PORT", "8080");
         return Integer.valueOf(port);
+    }
+
+    private static TemplateEngine getTemplateEngine() {
+        TemplateEngine templateEngine = new TemplateEngine();
+        templateEngine.addDialect(new LayoutDialect());
+        templateEngine.addDialect(new Java8TimeDialect());
+
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setPrefix("/templates");
+
+        templateEngine.addTemplateResolver(templateResolver);
+        return templateEngine;
     }
 }
